05920b4fe8a44aad0e3c44fd6933b506378668ba
MiguelRJ <miguel.rj96@gmail.com>
2017-12-13 14:35:04 +0100

    tablayout

---
+++ app/src/main/java/com/example/tablayout/ViewPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.tablayout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by usuario on 13/12/17.
+ */
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private int pageCount;
+
+    public ViewPagerAdapter(FragmentManager supportFragmentManager) {
+        super(supportFragmentManager);
+    }
+
+    public ViewPagerAdapter(FragmentManager supportFragmentManager, int pageCount) {
+        super(supportFragmentManager);
+        this.pageCount = pageCount;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        CustomFragment customFragment = null;
+        Bundle arguments = new Bundle();
+        switch (position){
+            case 0:
+                arguments.putString(CustomFragment.KEY_MESSAGE,"Fragment 1");
+                customFragment = customFragment.newInstance(arguments);
+                break;
+            case 1:
+                arguments.putString(CustomFragment.KEY_MESSAGE,"Fragment 2");
+                customFragment = customFragment.newInstance(arguments);
+                break;
+            case 2:
+                arguments.putString(CustomFragment.KEY_MESSAGE,"Fragment 3");
+                customFragment = customFragment.newInstance(arguments);
+                break;
+            case 3:
+                arguments.putString(CustomFragment.KEY_MESSAGE,"Fragment 4");
+                customFragment = customFragment.newInstance(arguments);
+                break;
+            case 4:
+                arguments.putString(CustomFragment.KEY_MESSAGE,"Fragment 5");
+                customFragment = customFragment.newInstance(arguments);
+                break;
+        }
+        return customFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
